633e41bcb75942ee3bb69e0dd329512c4c5d0828
xenkidu <aburton85@live.com>
2020-06-14 15:09:10 -0500

    Added Pages app.

--- products/models.py
+++ products/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    homedepot_url = models.TextField()
-    image_url = models.TextField()
-    price_chart = models.TextField()
+    title = models.CharField(max_length=120,)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    homedepot_url = models.CharField(max_length=240,)
+    image_url = models.CharField(max_length=240,)
+    price_chart = models.CharField(max_length=120,)
+    featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
