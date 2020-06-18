62337657825614955ffb6d4a61dc9bc39264ba06
xenkidu <aburton85@live.com>
2020-06-18 11:07:39 -0500

    Added templates

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    # return HttpResponse("<h1>Welcome to Home Depot Price History!</h1>")
+    return render(request, "home.html", {})
+def about_view(request, *args, **kwargs):
+    return HttpResponse("<h1>About Price History!</h1>")
