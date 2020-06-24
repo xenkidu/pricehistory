a24dd5bcb56b28dd757b50a1d6f41565faa47af7
xenkidu <aburton85@live.com>
2020-06-24 11:00:52 -0500

    added new pricehistory app

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from products.models import Product
 # Create your views here.
 
 
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Welcome to Home Depot Price History!</h1>")
+
     context = {
-        "title": "Home Page Title",
-        "description": "This app is built to give more insight and decision making power.",
-        "products": ['Ryobi Drill', 'Dewalt Drill', 'Saw', 'Hammer Drill']
+        'products': Product.objects.all()
     }
+
     return render(request, "home.html", context)
 
 
--- products/models.py
+++ products/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120,)
     description = models.TextField(blank=True)
-    price = models.DecimalField(decimal_places=2, max_digits=100)
     homedepot_url = models.CharField(max_length=240,)
     image_url = models.CharField(max_length=240,)
     price_chart = models.CharField(max_length=120,)
@@ -12,3 +11,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=12)
+    date = models.DateField(auto_now=True)
