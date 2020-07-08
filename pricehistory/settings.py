b5321519fcdb7da399b6013be27431af4a9c5474
xenkidu <aburton85@live.com>
2020-07-07 21:04:45 -0500

    Settings.

--- pricehistory/settings.py
+++ pricehistory/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8in$ch2)$vdbzkt(y9xgm^)=ob3_8#9g^$%4j5#^#!o$uiy1=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
